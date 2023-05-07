91a3001852787881d307849531e14591d86dc925
gravity0717 <1357vvv@naver.com>
2023-05-08 02:40:27 +0900

    make calib_camera_vanilla

--- calib_lines.py
+++ calib_lines.py
@@ -5,13 +5,124 @@ from scipy.spatial.transform import Rotation
 from numpy import random
 import cv2
 import matplotlib.pyplot as plt 
-from outlier_detect import outlier_iqr
+from outlier_detect import outlier_iqr, outlier_zscore
+
 
 
 
 def gaussian_noise(x, mu, std):
     x_noisy = [x[i] + random.normal(mu,std,size=x[0].shape) for i in range(len(x))]
     return x_noisy
+
+def calib_camera_vanilla(a, b, line_heigth):
+    assert len(a) == len(b) and len(a) > 0
+    
+    # Prepare 'a' and 'b' w.r.t. the paper
+    n = len(a)
+    center = np.array([(cam_w - 1) / 2, (cam_h - 1) / 2])
+    a = a - center
+    b = b - center
+    a = np.hstack((a, np.ones((n, 1)))) # To homogeneous notation
+    b = np.hstack((b, np.ones((n, 1)))) #    (n x 2) to (n x 3)
+
+    # Solve 'M @ v = 0 such that v > 0' in Equation (21)
+    M = np.zeros((3*(n-1), 2*n), dtype=a.dtype)
+    for i in range(1, n):
+        s, e = (3*(i-1), 3*i)
+        M[s:e, 0] = -a[0]
+        M[s:e, i] = a[i]
+        M[s:e, n] = b[0]
+        M[s:e, n+i] = -b[i]
+    
+    #Method 1) Using SVD
+    _, _, Vh = np.linalg.svd(M)
+    v = Vh[:][-1]
+    lm, mu = v[:n, np.newaxis], v[n:, np.newaxis]
+    
+    #assert (v > 0).all()
+      
+    #Calculate 'f' using Equation (24)
+  
+    c = mu * b - lm * a
+    d = lm * a - lm[0] * a[0]
+    f_n = sum([(ci[0]*di[0] + ci[1]*di[1]) * ci[2]*di[2] for ci, di in zip(c[1:], d[1:])])
+    f_d = sum([(ci[2]*di[2])**2 for ci, di in zip(c[1:], d[1:])])
+    #assert f_d > 0
+    #assert f_n < 0
+    sqrt = -f_n / f_d
+    f = np.sqrt(sqrt)
+    k= np.array([[f, 0., 0], [0., f, 0], [0., 0., 1.]]) 
+    
+    #Closed form of estimated r3
+    kinv = inv(k)
+    #Initialize kinv_cs
+    kinv_cs = np.zeros((len(c), 3, 1))
+    #Update kinv_cs 
+    kinv_cs = np.array([(kinv@cc) for cc in c]) # N * (3x1) 
+    #Transform 3*1 vector to 3*3 matrix which is equivalnt to cross product  
+    crs_kinv_cs =[]
+    
+    for kinv_c in kinv_cs:
+        #Represent cross product matrix as a three by three skew-symmetric matrices for matrix multiplication
+        crossed = np.array([[0,-kinv_c[2],kinv_c[1]],
+                        [kinv_c[2],0,-kinv_c[0]],
+                        [-kinv_c[1],kinv_c[0],0]])
+        crs_kinv_cs.append(crossed)
+    
+    #Power that matrix 
+    crs_kinv_cs_2 = np.array([crs_kinv_c @ crs_kinv_c for crs_kinv_c in crs_kinv_cs])
+    
+    # sum all N matrices , like A1 + A2 + A3.... 
+    A = np.zeros_like(crs_kinv_cs[0])
+    for element in crs_kinv_cs_2:
+        A = A + element   
+
+    # r3 can be computed as the eigenvector of this formula(A) corresponding to the smallest eigenvalue 
+    _,_,Vh = svd(-A)
+    r3 =Vh[:][-1]
+    
+    #Solve rvec
+    theta = np.arccos(r3[2])
+    phi = np.arctan(-r3[0]/r3[1])
+    R = Rz(phi)@Rx(theta)
+
+    #Closed form of estimated length of line segment
+    l = r3.T@sum(kinv_cs)/n
+
+    #Calculate p and q to get a position value 
+    Q =np.diag([1,1,-1]) 
+    pq = []
+    for i in range(n):
+        lma = lm[i]*a[i]
+        lma = lma[:,np.newaxis]
+        mub = mu[i]*b[i]
+        mub = mub[:,np.newaxis]
+        ij = Q @ R.T @ kinv @ np.hstack((lma,mub))
+        pq.append(ij)
+    p = [p[:,0]  for p in pq]
+    q = [q[:,1]  for q in pq]
+    
+    #Calculate position
+    x = [0.5*(p[i][0]+q[i][0]) for i in range(n)]
+    y = [0.5*(p[i][1]+q[i][1]) for i in range(n)]
+    p_3 =[ p[i][2] for i in range(n)]
+    q_3 =[ q[i][2] for i in range(n)]
+    height = sum(p_3+q_3)/(2*n) + l/2
+    
+    
+    #Scaling to get absolute size 
+    height = height*(line_heigth/l)
+    
+    #Ignore that positive or negative 
+    height = abs(height)
+
+    # result 
+    result ={'f': f, 
+             'theta': theta, 
+             'phi' : phi, 
+             'height': height}
+    return result
+    
 
 def calib_camera_ransac(a, b,
                         line_height=None,
@@ -300,6 +411,137 @@ def calib_camera_nlines_ransac(a,b,
     result['height'] = height
     result['viz'] = viz
     return result
+
+
+
+
+def calib_camera_stat(a, b, iqr = True, line_height=None, **config):
+    assert len(a) == len(b) and len(a) > 0
+    
+    # Prepare 'a' and 'b' w.r.t. the paper
+    n = len(a)
+    center = np.array([(config["cam_w"] - 1) / 2, (config["cam_h"] - 1) / 2])
+    a = a - center
+    b = b - center
+    a = np.hstack((a, np.ones((n, 1)))) # To homogeneous notation
+    b = np.hstack((b, np.ones((n, 1)))) #    (n x 2) to (n x 3)
+    
+    # Solve 'M @ v = 0 such that v > 0' in Equation (21)
+    M = np.zeros((3*(n-1), 2*n), dtype=a.dtype)
+    for i in range(1, n):
+        s, e = (3*(i-1), 3*i)
+        M[s:e, 0] = -a[0]
+        M[s:e, i] = a[i]
+        M[s:e, n] = b[0]
+        M[s:e, n+i] = -b[i]
+        
+    # Solve Mv = 0 by SVD 
+    _, _, Vh = np.linalg.svd(M)
+    v = Vh[:][-1]
+    lm, mu = v[:n, np.newaxis], v[n:, np.newaxis]
+
+    # To detect outlier, divide both 
+    lm_mu = lm / mu 
+    if iqr: 
+        outlier_index= outlier_iqr(lm_mu)
+    else: 
+        outlier_index= outlier_zscore(lm_mu)
+   
+    # Delete Outliers 
+    lm = np.array([lm[i] for i in range(n) if i not in outlier_index])
+    mu = np.array([mu[i] for i in range(n) if i not in outlier_index])
+    a = np.array([a[i] for i in range(n) if i not in outlier_index])
+    b = np.array([b[i] for i in range(n) if i not in outlier_index])
+
+    # Recalculate the number of inliers
+    n = len(a)
+
+    #Calculate 'f' using Equation (24) 
+    c = mu * b - lm * a
+    d = lm * a - lm[0] * a[0]
+    f_n = sum([(ci[0]*di[0] + ci[1]*di[1]) * ci[2]*di[2] for ci, di in zip(c[1:], d[1:])])
+    f_d = sum([(ci[2]*di[2])**2 for ci, di in zip(c[1:], d[1:])])
+    #assert f_d > 0
+    #assert f_n < 0
+    sqrt = -f_n / f_d
+    f = np.sqrt(sqrt)
+    k= np.array([[f, 0., 0], [0., f, 0], [0., 0., 1.]]) 
+    
+    #Closed form of estimated r3
+    kinv = inv(k)
+    #Initialize kinv_cs
+    kinv_cs = np.zeros((len(c), 3, 1))
+    #Update kinv_cs 
+    kinv_cs = np.array([(kinv@cc) for cc in c]) # N * (3x1) 
+    #Transform 3*1 vector to 3*3 matrix which is equivalnt to cross product  
+    crs_kinv_cs =[]
+    
+    for kinv_c in kinv_cs:
+        #Represent cross product matrix as a three by three skew-symmetric matrices for matrix multiplication
+        crossed = np.array([[0,-kinv_c[2],kinv_c[1]],
+                        [kinv_c[2],0,-kinv_c[0]],
+                        [-kinv_c[1],kinv_c[0],0]])
+        crs_kinv_cs.append(crossed)
+    
+    #Power that matrix 
+    crs_kinv_cs_2 = np.array([crs_kinv_c @ crs_kinv_c for crs_kinv_c in crs_kinv_cs])
+    
+    # sum all N matrices , like A1 + A2 + A3.... 
+    A = np.zeros_like(crs_kinv_cs[0])
+    for element in crs_kinv_cs_2:
+        A = A + element   
+
+    # r3 can be computed as the eigenvector of this formula(A) corresponding to the smallest eigenvalue 
+    _,_,Vh = svd(-A)
+    r3 =Vh[:][-1]
+    
+    #Solve rvec
+    theta = np.arccos(r3[2])
+    phi = np.arctan(-r3[0]/r3[1])
+    Rx = np.array([[1., 0, 0], [0, np.cos(theta), -np.sin(theta)], [0, np.sin(theta), np.cos(theta)]])
+    Rz = np.array([[np.cos(phi), -np.sin(phi), 0], [np.sin(phi), np.cos(phi), 0], [0, 0, 1.]])
+    R = Rz@Rx
+
+    #Closed form of estimated length of line segment
+    l = r3.T@sum(kinv_cs)/n
+
+    #Calculate p and q to get a position value 
+    Q =np.diag([1,1,-1]) 
+    pq = []
+    for i in range(n):
+        lma = lm[i]*a[i]
+        lma = lma[:,np.newaxis]
+        mub = mu[i]*b[i]
+        mub = mub[:,np.newaxis]
+        ij = Q @ R.T @ kinv @ np.hstack((lma,mub))
+        pq.append(ij)
+    p = [p[:,0]  for p in pq]
+    q = [q[:,1]  for q in pq]
+    
+    # Calculate the heigth
+    p_3 =[ p[i][2] for i in range(n)]
+    q_3 =[ q[i][2] for i in range(n)]
+    height = sum(p_3+q_3)/(2*n) + l/2
+  
+    #Scaling to get absolute size 
+    height = height*(line_height/l)
+    
+    #Ignore that positive or negative 
+    height = abs(height)
+
+    # result 
+    result = dict()
+    result['f'] = f
+    result['theta'] =theta
+    result['phi'] = phi
+    result['height'] = height
+ 
+    return result
+
+
+
+
+
 
 if __name__ == "__main__":
    
