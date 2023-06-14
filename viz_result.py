8416745bd48b96b8f51fdacd3ddcd38240f0a00b
gravity0717 <1357vvv@naver.com>
2023-06-14 21:33:25 +0900

    viz_result completed

--- viz_result.py
+++ viz_result.py
@@ -9,14 +9,15 @@ import matplotlib.pyplot as plt
 def visualize_result(x_value:np.array,
                     y_value:dict,
                     title:str):
-    methods=["IQR",
-    "RANSAC_IQR",
-    "RANSAC_IQR_2",
-    "Vanilla model",
-    "ZSCORE",
-    "RANSAC_ZSCORE",
-    "RANSAC_ZSCORE_2"]
+    methods = ["IQR",
+                "RANSAC_IQR",
+                "RANSAC_IQR_2",
+                "Vanilla model",
+                "ZSCORE",
+                "RANSAC_ZSCORE",
+                "RANSAC_ZSCORE_2"]
 
+    linestyles =  [ '--', '-.', ':', '-', 'solid', 'dashed', 'dashdot', 'dotted']
     params = ["f",
               "theta",
               "phi",
@@ -26,14 +27,30 @@ def visualize_result(x_value:np.array,
     plt.figure()
 
     for i, param in enumerate(params):
-        plt.subplot(int(len(params)/2)+1, int(len(params)/2),i+1)
-        for _ , method in enumerate(methods):
-            plt.plot(x_value, y_value[method][param], label = method)
-            if i == 0:
-                plt.legend(fontsize = 7, bbox_to_anchor = (1,-1.5))
-            else: pass 
+
+        # Extract first subplot to get legend
+        if i == 0:
+            ax1 = plt.subplot(int(len(params))/2, int(len(params))/2, i+1)
+        else: 
+            plt.subplot(int(len(params))/2, int(len(params))/2, i+1)
+        
+        for j , method in enumerate(methods):
+            # plot graph
+            plt.plot(x_value, y_value[method][param], linestyle = linestyles[j], label = method)
+
+            # Configuration of axises
+            plt.xlabel('number of line segments')
             plt.ylim([0,100])
-            plt.ylabel(f"accuarcy of {param}")
+            plt.ylabel(f"Error of {param}")
+            
+    # Adjust width spaces and horizontal space between subplots
+    plt.subplots_adjust(wspace = 0.3, hspace = 0.5) 
+
+    # Legend 
+    handles, labels = ax1.get_legend_handles_labels()
+    plt.figlegend(handles, labels, fontsize = 7, loc = "upper center", ncol = 4)
+
+    # Save
     plt.savefig(f"result/{title}1.png")
 
 
@@ -42,5 +59,5 @@ if __name__ == "__main__":
         x = json.load(f)
         x = x["median"]
     
-    visualize_result(np.arange(2,400),x,"Synthetic data experiment")
+    visualize_result(np.arange(2,40),x,"Synthetic data experiment")
     
